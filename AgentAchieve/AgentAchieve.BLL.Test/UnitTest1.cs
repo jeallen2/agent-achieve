72f28df47998682964dc7dc96be7a22e5a2b8183
Jeffrey Allen <jra5757@psu.edu>
2024-01-28 10:18:27 -0500

    #21 Added Test Projects

---
+++ AgentAchieve/AgentAchieve.BLL.Test/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace AgentAchieve.BLL.Test
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
---
+++ AgentAchieve/AgentAchieve.DAL.Test/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace AgentAchieve.DAL.Test
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
