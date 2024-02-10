80a48ccd04fed9e79d2dc705584d5a99ba117ce0
Jeffrey Allen <jra5757@psu.edu>
2024-02-10 14:27:56 -0500

    #22 Test entity creation, run migration and apply to the database.

---
+++ AgentAchieve/AgentAchieve.Core/Domain/Client.cs
@@ -0,0 +1,18 @@
+﻿namespace AgentAchieve.Core.Domain
+{
+    /// <summary>
+    /// Represents a client entity.
+    /// </summary>
+    public class Client : IEntity<int>
+    {
+        /// <summary>
+        /// Gets or sets the client ID.
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// Gets or sets the client name.
+        /// </summary>
+        public string? Name { get; set; }
+    }
+}
--- AgentAchieve/AgentAchieve.Infrastructure/Data/ApplicationDbContext.cs
+++ AgentAchieve/AgentAchieve.Infrastructure/Data/ApplicationDbContext.cs
@@ -1,10 +1,21 @@
+using AgentAchieve.Core.Domain;
+using AgentAchieve.Infrastructure.Data.Configurations;
 using AgentAchieve.Infrastructure.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using System.Reflection;
 
 namespace AgentAchieve.Infrastructure.Data
 {
     public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext<ApplicationUser>(options)
     {
+
+        public DbSet<Client> Clients { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            builder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+        }
     }
 }
